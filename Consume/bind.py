885cdae9341ca549d616c41fbf2fbfd0875593db
lvjianshu <1020094189@qq.com>
2019-12-30 16:10:42 +0800

    1227

--- Consume/bind.py
+++ Consume/bind.py
@@ -6,7 +6,7 @@ import pika
 class bind:
     def __init__(self):
         channel = RABBITMQ = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq['host'], port=int(rabbitmq['port']), credentials=pika.PlainCredentials(password=rabbitmq['password'], username=rabbitmq['user']))).channel()
-        channel.exchange_declare(exchange=exchange, exchange_type='topic', durable=True)
+        channel.exchange_declare(exchange=exchange, exchange_type='topic', durable=False)
         binding_key = 'data.msm'
         channel.queue_declare(queue=queue, durable=True)
         channel.queue_bind(exchange=exchange, queue=queue, routing_key=binding_key)
